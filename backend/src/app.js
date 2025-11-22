55854eb2f38a1040b7160e566d3c31cf157a2e7d
Nabin Ghimire <ghimiren2057@gmail.com>
2025-11-22 19:39:26 +0545

    cors disabled for test deploy

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,11 +14,18 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// app.use(
+//     cors({
+//         origin: ['http://localhost:5173'],
+//         credentials: true,
+//     }),
+// );
+
 app.use(
     cors({
-        origin: ['http://localhost:5173'],
+        origin: "*",
         credentials: true,
-    }),
+    })
 );
 
 app.use('/api/auth', authRoute);
